b1d632fa4868d7136a7b910c1366c0336b3a09fb
drozd <drozd.dmitriy.etc@gmail.com>
2019-06-11 12:15:21 +0300

    Rename moduledb in module_db

---
+++ core/module_db.rb
@@ -0,0 +1,15 @@
+module Database
+  FILE_NAME = 'data.yml'.freeze
+
+  def load_entities
+    raise BookValidation, 'file not found!' unless File.exist?(FILE_NAME)
+
+    (YAML.load_file FILE_NAME) || { authors: nil, books: nil, orders: nil, readers: nil }
+  end
+
+  def save(data)
+    raise BookValidation, 'file not found!' unless File.exist?(FILE_NAME)
+
+    File.open(FILE_NAME, 'w') { |file| file.write(data.to_yaml) }
+  end
+end
